5b0852a1470f747b9b11dcea36bbae4d6052d80d
root <root@vmi3096184.contaboserver.net>
2026-04-15 21:08:27 +0200

    Upgrade to scientific calculator

--- script.js
+++ script.js
@@ -14,8 +14,25 @@ function deleteLast() {
 
 function calculate() {
     try {
-        display.value = eval(display.value);
+        let result = eval(display.value);
+        display.value = Number.isInteger(result) ? result : result.toFixed(8).replace(/\.?0+$/, "");
     } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+function scientific(func) {
+    try {
+        let val = eval(display.value) || 0;
+        switch(func) {
+            case 'sin': display.value = Math.sin(val); break;
+            case 'cos': display.value = Math.cos(val); break;
+            case 'tan': display.value = Math.tan(val); break;
+            case 'log': display.value = Math.log10(val); break;
+            case 'sqrt': display.value = Math.sqrt(val); break;
+            case 'pi': display.value += Math.PI; break;
+        }
+    } catch (e) {
         display.value = 'Error';
     }
 }
